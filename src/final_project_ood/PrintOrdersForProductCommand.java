68ed0d97a5b91240e62c8db9b0458b9c095cc671
giladf424 <giladf424@gmail.com>
2024-04-12 12:24:54 +0300

    Options 4.7 & 4.9

--- src/final_project_ood/PrintOrdersForProductCommand.java
+++ src/final_project_ood/PrintOrdersForProductCommand.java
@@ -2,12 +2,12 @@ package final_project_ood;
 
 import java.util.Iterator;
 
-public class PrintProductCommand implements ICommand {
+public class PrintOrdersForProductCommand implements ICommand{
 	private StorageManager storageManager;
 	private OrderManager orderManager;
 	private String productId;
 	
-	public PrintProductCommand (String productID) {
+	public PrintOrdersForProductCommand(String productID) {
 		this.storageManager = Store.getStoreInstance().getStorageManager();
 		this.orderManager = Store.getStoreInstance().getOrderManager();
 		this.productId = productID;
@@ -16,24 +16,27 @@ public class PrintProductCommand implements ICommand {
 	@Override
 	public void execute() {
 		int profit = 0;
+		int totalProfit = 0;
 		Product product = storageManager.getProductByID(productId);
-		if(product == null || product.getIsActive() == false) {
+		if (product == null) {
 			System.out.println("No product with this ID.");
 			return;
+		}else if (!product.getIsActive()) {
+			System.out.println("The product you chose isn't available in the store anymore.");
 		}
-		System.out.println(product.toString());
+		System.out.println("This is the list of all orders of the product you chose:");
 		for (Iterator<String> iterator = product.productOrdersID.iterator(); iterator.hasNext();) {
 			String orderID = (String) iterator.next();
 			Order order = orderManager.getOrderByID(orderID);
 			if (order != null) {
 				System.out.println(order.toString());
-				profit += orderManager.calculateOrderProfit(order, product.sellingPrice, product.costPrice, product.currency);
+				profit = orderManager.calculateOrderProfit(order, product.sellingPrice, product.costPrice, product.currency);
+				totalProfit += profit;
+				System.out.println("The profit from this order is: " + profit + " NIS");
 			}
 		}
-		System.out.println("Total profit from product's orders: " + profit + " NIS");
+		System.out.println("Total profit from product's orders: " + totalProfit + " NIS");
 		
 	}
-	
-	
 
 }
